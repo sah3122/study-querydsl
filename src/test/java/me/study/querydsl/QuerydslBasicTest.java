0dcc408325e52a9d0400f306f9931f838151dcd3
dongchul <sah3122@naver.com>
2020-03-11 18:03:50 +0900

    Add Constant & Concat

--- src/test/java/me/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/me/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package me.study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import me.study.querydsl.entity.Member;
@@ -413,5 +414,29 @@ public class QuerydslBasicTest {
                     .otherwise("기타"))
                 .from(member)
                 .fetch();
+    }
+
+    @Test
+    void constant() {
+        List<Tuple> a = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+        for (Tuple tuple : a) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    void concat() {
+        List<String> fetch = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : fetch) {
+            System.out.println("s = " + s);
+        }
     }
 }
