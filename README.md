46cfe8314f386c5e0727c6ab4a59cb75d2531f72
dongchul <sah3122@naver.com>
2020-03-15 21:37:21 +0900

    Add BooleanBuilder

--- README.md
+++ README.md
@@ -118,5 +118,9 @@
                 * username.as("memberName") : 필드에 별칭 적용
     * @QueryProjection 활용
         * 이 방법은 컴파일러로 타입을 체크 할 수 있으므로 가장 안전한 방법이다. 다만 DTO에 QueryDSL 어노테이션을 유지해야 하는 점과 DTO까지 Q 파일을 생성해야하는 단점이 있다.
+    * 동적 쿼리 - booleanBuilder 사용
+        * 동적 쿼리를 해결하는 두가지 방식
+            * BooleanBuilder
+            * Where 다중 파라미터 사용
         
     
--- src/test/java/me/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/me/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package me.study.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -543,5 +544,31 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    void dynamicQueryBooleanBuilder() {
+        String username = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(username, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
