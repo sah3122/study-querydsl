28670d4679b643e6da32bfdf1e21b145b58e54a1
dongchul <sah3122@naver.com>
2020-03-24 21:28:15 +0900

    Add refacotr SpringDataJPA

---
+++ src/test/java/me/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package me.study.querydsl.repository;
+
+import me.study.querydsl.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all).containsExactly(member);
+
+        List<Member> member1 = memberRepository.findByUsername("member1");
+        assertThat(member1).containsExactly(member);
+    }
+}
