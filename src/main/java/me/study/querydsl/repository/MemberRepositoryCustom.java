74b913ad81f09afd928bed9c8d09a636170dfa09
dongchul <sah3122@naver.com>
2020-03-25 00:08:23 +0900

    Add SpringDataPaging

--- src/main/java/me/study/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/me/study/querydsl/repository/MemberRepositoryCustom.java
@@ -2,9 +2,13 @@ package me.study.querydsl.repository;
 
 import me.study.querydsl.dto.MemberSearchCondition;
 import me.study.querydsl.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface MemberRepositoryCustom {
     List<Member> search(MemberSearchCondition condition);
+    Page<Member> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+    Page<Member> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
 }
--- src/main/java/me/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/me/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,10 +1,14 @@
 package me.study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import me.study.querydsl.dto.MemberSearchCondition;
 import me.study.querydsl.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -30,6 +34,58 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         ageBetween(condition.getAgeLoe(), condition.getAgeLoe())
                 )
                 .fetch();
+    }
+
+    @Override
+    public Page<Member> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()),
+                        ageBetween(condition.getAgeLoe(), condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();// contents용과 count용 쿼리를 두번 날리게 된다. 페이징 기능에선 이걸 쓸것
+        List<Member> content = results.getResults(); // 실제 쿼리 내역
+        long total = results.getTotal(); // 전체 수
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<Member> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<Member> members = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()),
+                        ageBetween(condition.getAgeLoe(), condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()),
+                        ageBetween(condition.getAgeLoe(), condition.getAgeLoe())
+                )
+                .fetchCount();
+
+        return new PageImpl<>(members, pageable, total); // content 쿼리랑 실제 쿼리랑 분리를 해서 카운트 쿼리를 최적화 할 수 있다.
     }
 
     private BooleanExpression ageBetween(int ageLoe, int ageGoe) {
