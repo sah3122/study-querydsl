41d357133cdaf97499eee7a4c25b822e59d547f6
dongchul <sah3122@naver.com>
2020-03-25 13:33:18 +0900

    Add SpringDataPaging on Controller

--- src/main/java/me/study/querydsl/controller/MemberController.java
+++ src/main/java/me/study/querydsl/controller/MemberController.java
@@ -3,7 +3,11 @@ package me.study.querydsl.controller;
 import lombok.RequiredArgsConstructor;
 import me.study.querydsl.dto.MemberSearchCondition;
 import me.study.querydsl.dto.MemberTeamDto;
+import me.study.querydsl.entity.Member;
 import me.study.querydsl.repository.MemberJpaRepository;
+import me.study.querydsl.repository.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +17,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberRepository memberRepository;
     private final MemberJpaRepository memberJpaRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<Member> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<Member> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
